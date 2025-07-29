89fd72bf412949364c3153782bd270c0efa285d8
jtaguchi <12386296+jtaguchi@users.noreply.github.com>
2025-07-29 06:40:11 +0000

    Add print_env_vars

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::env;
 
-fn main() {
+fn print_env_vars() {
     for (var_key, var_val) in env::vars() {
         println!("{var_key}={var_val}");
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        print_env_vars();
     }
 }
